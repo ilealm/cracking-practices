c6498a17af127cdcab71ffb75b0a45f39b3d0fe6
Iris Leal <iris_leal_m@hotmail.com>
2020-08-18 16:55:50 -0700

    Added string_rotation.string_rotation method

---
+++ cracking_practices/string_rotation/string_rotation.py
@@ -0,0 +1,33 @@
+# PROBLEM DOMAIN
+# String Rotation 1.9 : Assume you have a method isSubstring which checks if one word is a substring of another.(I'm not ussing this)
+# Give two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstrig.
+# s1 = waterbottle
+# s2 = erbottlewat
+
+
+# Approach 1, Traverse s2 until I find the start of s1, concatenate the rest of the letters, then check is they are the same word.
+def string_rotation(original_str,lookup_str):
+    if len(original_str)==0 or len(lookup_str) == 0 : return False
+    if not len(original_str) == len(lookup_str) : return False
+    original_str = original_str.lower()
+    lookup_str = lookup_str.lower()
+
+    for i in range(0,len(lookup_str)-1):
+        if lookup_str[i] == original_str[0]:
+            string_to_review = lookup_str[i:] + lookup_str[0:i]
+            print(string_to_review)
+            if original_str == string_to_review:
+                return True
+
+    return False
+
+
+
+if __name__ == "__main__":
+    original_str = 'waterbottle'
+    lookup_str = 'erbottlewat'
+
+    original_str = 'rainbow'
+    lookup_str = 'bowrain'
+
+    print(string_rotation(original_str,lookup_str))

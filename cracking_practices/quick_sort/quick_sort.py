22489f9e95fac36501f3654d07ac5e4409289b3a
Iris Leal <iris_leal_m@hotmail.com>
2020-08-26 16:17:19 -0700

    Almost ready

--- cracking_practices/quick_sort/quick_sort.py
+++ cracking_practices/quick_sort/quick_sort.py
@@ -1,19 +1,22 @@
 
 
 def quick_sort(array, left, right):
-    # base case
-    # if left >= right:
-    # if left + right <= 1:
-    #     return
-    # set partition
+
+
+    if left >= right:
+        return
+
     pivot_index = partition(array, left, right)
 
-    if left < pivot_index - 1:
+    if left < pivot_index - 1:  # this if helps to exit the recursion
         quick_sort(array, left, pivot_index-1)
     if pivot_index < right:
         quick_sort(array, pivot_index, right)
 
     return array
+
+
+
 
 
 def partition(array, left, right):
@@ -29,16 +32,22 @@ def partition(array, left, right):
         while array[right] > pivot:
             right -=1
 
-        # swap left and right
-        array[left], array[right] = array[right], array[left]
+        # swap left and right, if are in the reverse order
+        # if array[left] < array[right]:
+        if left < right:
+            array[left], array[right] = array[right], array[left]
         left += 1
         right -= 1
 
     return left
 
 
-
-
 if __name__ == "__main__":
-    test = [19, 220, 63, 105, 2, 46]
+    # test = [19, 220, 63, 105, 2, 46, 50] # works
+    test = [19, 220, 63, 105, 2, 46] # dont works
+    # test = [19, 22, 63, 105, 2, 46]
+    # test = [10, 15, -3, 12, 1, 30, 7]
     print (quick_sort(test, 0, len(test)-1))
+    # quick_sort(test, 0, len(test)-1)
+    # print (quick_sort_craking(test, 0, len(test)-1))
+    # print (test)

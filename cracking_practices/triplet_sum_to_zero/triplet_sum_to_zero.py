042b01c9b594801b8fff7dbf6f91e9f9a9f560bb
Iris Leal <iris_leal_m@hotmail.com>
2020-11-24 17:20:52 -0800

    Ready

--- cracking_practices/triplet_sum_to_zero/triplet_sum_to_zero.py
+++ cracking_practices/triplet_sum_to_zero/triplet_sum_to_zero.py
@@ -1,11 +1,10 @@
+# Triplet Sum Close to Zero
+# Given an array of unsorted numbers, find all unique triplets in it that add up to zero.
+
 # Input: [-3, 0, 1, 2, -1, 1, -2]
 # Output: [-3, 1, 2], [-2, 0, 2], [-2, 1, 1], [-1, 0, 1]
 # Explanation: There are four unique triplets whose sum is equal to zero.
-# Example 2:
 
-# Input: [-5, 2, -1, -2, 3]
-# Output: [[-5, 2, 3], [-2, -1, 3]]
-# Explanation: There are two unique triplets whose sum is equal to zero.
 
 # I will have 3 pointers, then I will check if the sum of them is 0, if so, then I will move
 def search_triplets(arr):
@@ -26,7 +25,6 @@ def search_triplets(arr):
                     # triplets.append([arr[current], arr[i], arr[j]])
                     # BUT the problem wants unique triplets, I will check if is not already there
                     founded = [arr[current], arr[i], arr[j]]
-
                     if not founded in triplets:
                         triplets.append(founded)
 

391e133724d667f21778a16f9c4e260e39180681
Iris Leal <iris_leal_m@hotmail.com>
2020-12-09 13:54:14 -0800

    reviewing Educative solution

--- cracking_practices/testing_reviews.py
+++ cracking_practices/testing_reviews.py
@@ -1,25 +1,60 @@
-def find_happy_number(num):
-  slow, fast = num, num
-  while True:
-    slow = find_square_sum(slow)  # move one step
-    fast = find_square_sum(find_square_sum(fast))  # move two steps
-    if slow == fast:  # found the cycle
-      break
-  return slow == 1  # see if the cycle is stuck on the number '1'
+class Node:
+  def __init__(self, value, next=None):
+    self.value = value
+    self.next = next
 
 
-def find_square_sum(num):
-  _sum = 0
-  while (num > 0):
-    digit = num % 10
-    _sum += digit * digit
-    num //= 10
-  return _sum
+def is_palindromic_linked_list(head):
+  if head is None or head.next is None:
+    return True
+
+  # find middle of the LinkedList
+  slow, fast = head, head
+  while (fast is not None and fast.next is not None):
+    slow = slow.next
+    fast = fast.next.next
+
+  head_second_half = reverse(slow)  # reverse the second half
+  # store the head of reversed part to revert back later
+  copy_head_second_half = head_second_half
+
+  # compare the first and the second half
+  while (head is not None and head_second_half is not None):
+    if head.value != head_second_half.value:
+      break  # not a palindrome
+
+    head = head.next
+    head_second_half = head_second_half.next
+
+  reverse(copy_head_second_half)  # revert the reverse of the second half
+
+  if head is None or head_second_half is None:  # if both halves match
+    return True
+
+  return False
+
+
+def reverse(head):
+  prev = None
+  while (head is not None):
+    next = head.next
+    head.next = prev
+    prev = head
+    head = next
+  return prev
 
 
 def main():
-  print(find_happy_number(23))
-  print(find_happy_number(12))
+  head = Node(2)
+  head.next = Node(4)
+  head.next.next = Node(6)
+  head.next.next.next = Node(4)
+  head.next.next.next.next = Node(2)
+
+  print("Is palindrome: " + str(is_palindromic_linked_list(head)))
+
+  head.next.next.next.next.next = Node(2)
+  print("Is palindrome: " + str(is_palindromic_linked_list(head)))
 
 
 main()

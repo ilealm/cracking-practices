3f31bfed2711be4139c0d0d183b0ec73664e3b75
Iris Leal <iris_leal_m@hotmail.com>
2020-12-10 13:32:45 -0800

    Ready step 2, now the second half of the LL is reversed

--- cracking_practices/rearrange_linkedlist/rearrange_linkedlist.py
+++ cracking_practices/rearrange_linkedlist/rearrange_linkedlist.py
@@ -30,48 +30,73 @@ def get_middle_node(head):
     return [slower, middle, middle % 2 == 0]
 
 
-def reversev1(head):
-    main_head = head
+# def reverse_all_linkedlist(head):
+#     prev = None
+
+#     while head:
+#         moving = head
+#         next_ = moving.next
+#         moving.next = prev
+#         prev = moving
+#         head = next_
+
+#     # Finally, I need to set the head to this finally moved node
+#     head = moving
+
+#     return head
+
+# helper function that returns the k node of a LL
+def get_k_node(head, k):
+    current_node = head
+    i = 1
+
+    while current_node and i < k:
+        current_node = current_node.next
+        i += 1
+
+    return current_node
+
+
+# This functions reverse only a part of a singly ll.
+def reverse_half_linkedlist(head, start_node, start_position):
+
     prev = None
-    while head:
-        moving = head
-        head = head.next
+    # break the first half of the ll, so I can rejoin with the reversed half
+    end_first_half = get_k_node(head, start_position-1)
+    end_first_half.next = None
+
+
+    # reverse the second half
+    while start_node:
+        next_ = start_node.next
+        start_node.next = None
+        moving = start_node
+
         moving.next = prev
         prev = moving
+        start_node = next_
 
-    head = moving
+    # join halves
+    end_first_half.next = moving
 
+    # return head
 
-def reverse(head):
-    prev = None
-
-    while head:
-        moving = head
-        next_ = moving.next
-        moving.next = prev
-        prev = moving
-        head = next_
-
-    # Finally, I need to set the head to this finally moved node
-    head = moving
-
-    return head
-
-    # head.print_list()
 
 
 # My approach for this problem will the tackle in 3 parts: find the middle, reverse in place the second half of the LL,
 # make the zip o both halfs in place
 def reorder(head):
-    # This problem
-    # TODO: Write your code here
+    # Step 1: obtain the middle
     middle_values = get_middle_node(head)
     # the middle value is not that acurate, BC I'm not traversing all the LL to be sure from all the nodes to the right of
     # the middle
-    middle, position, is_even = middle_values[0], middle_values[1], middle_values[2]
+    middle_node, position, is_even = middle_values[0], middle_values[1], middle_values[2]
 
-    print("middle node: ", middle.value, "position: ", position)
-    reverse(head)
+    # Step 2: reverse the 2nd half or the array
+    print("middle node: ", middle_node.value, "position: ", position)
+    reverse_half_linkedlist(head, middle_node, position)
+
+    print(head.print_list())
 
     return
 
@@ -92,11 +117,12 @@ def main():
     head.next.next.next.next = Node(5)
     head.next.next.next.next.next = Node(6)
 
-    # reorder(head)
-    head = reverse(head)
-    print("in main....")
+    reorder(head)
+    # set the head to the new node
+    # head = reverse_linkedlist(head)
+    # print("in main....")
     head.print_list()
-    print('here')
+    # print("here")
 
 
 main()

949ba1d6943abf736619d9f9085fe4e9820a6bd9
Iris Leal <iris_leal_m@hotmail.com>
2021-01-05 11:17:03 -0800

    First approach

---
+++ cracking_practices/employee_free_time/README.md
@@ -0,0 +1,53 @@
+[Table of Contents](../../README.md)
+
+# Problem
+
+<!-- [Whiteboard approach](x_x) -->
+
+### PROBLEM DOMAIN
+
+### VISUALS
+
+```
+
+```
+
+### EDGE CASES
+
+- I can have valid values in the arays.
+- Values can be repeted.
+- I will assume that each pair or list is a 2 shifts employees's working time in a day, so 4 lists means 2 employees schedules.
+
+
+### ALGORITHMS
+
+#### APPROACH 1,
+
+```
+algo
+
+```
+
+#### TESTS
+
+```
+Please review the test section below.
+```
+
+#### BIG O
+
+**Time O(n):** I need to
+
+**Space O(n):** I'm
+
+### CODE
+
+[cracking_practices/x_x/x_x.py](x_x.py)
+
+### TESTS
+
+[tests/test_x_x.py](../../tests/test_x_x.py)
+
+### GITHUB BRANCH
+
+[Pull Request # n, Branch: x_x](https://github.com/ilealm/cracking-practices/pull/X)
---
+++ cracking_practices/employee_free_time/employee_free_time.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+
+
+class Interval:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def print_interval(self):
+        print("[" + str(self.start) + ", " + str(self.end) + "]", end="")
+
+
+def find_employee_free_time(schedule):
+    result = []
+    # just to make easier to read
+    shiftA = start = employeeA = 0
+    shiftB = end = employeeB = 1
+
+    # get the gaps of each employee
+    gaps = []
+    for i in range(len(schedule)):
+        gaps.append([schedule[i][shiftA].end, schedule[i][shiftB].start])
+
+    # get the common gaps of both employees
+    start_gap = max(gaps[employeeA][start], gaps[employeeB][start])
+    end_gap = min(gaps[employeeA][end], gaps[employeeB][end])
+    result.append([start_gap, end_gap])
+    # result.append(end_gap)
+
+    # validate if employeeA and employeeb have a touching gap
+    if (schedule[employeeB][shiftB].end + 1) == schedule[employeeA][shiftB].start:
+        result.append([schedule[employeeB][shiftB].end, schedule[employeeA][shiftB].start])
+        # result.append(schedule[employeeB][shiftB].end)
+        # result.append(schedule[employeeA][shiftB].start)
+
+    return result
+
+
+def main():
+
+    input = [[Interval(1, 3), Interval(5, 6)], [Interval(2, 3), Interval(6, 8)]]
+    # print("Free intervals: ", end="")
+    print(find_employee_free_time(input))
+    # for interval in find_employee_free_time(input):
+    #     interval.print_interval()
+    # print()
+
+    input = [[Interval(1, 3), Interval(9, 12)], [Interval(2, 4), Interval(6, 8)]]
+    print("Free intervals: ", end='')
+    print(find_employee_free_time(input))
+    # for interval in find_employee_free_time(input):
+    #     interval.print_interval()
+    # print()
+
+    # input = [[Interval(1, 3)], [
+    #     Interval(2, 4)], [Interval(3, 5), Interval(7, 9)]]
+    # print("Free intervals: ", end='')
+    # for interval in find_employee_free_time(input):
+    #     interval.print_interval()
+    # print()
+
+
+main()

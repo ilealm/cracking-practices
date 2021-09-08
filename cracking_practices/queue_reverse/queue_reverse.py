3fcfba6f4fa566658d7ac4236ac0a23b0c5d8b6c
Iris Leal <iris_leal_m@hotmail.com>
2021-09-08 14:57:48 -0700

    Ready base methods for Queue./Users/ileal/myProjects/cracking-practices/.venv/bin/python

--- cracking_practices/queue_reverse/queue_reverse.py
+++ cracking_practices/queue_reverse/queue_reverse.py
@@ -43,5 +43,50 @@ class Queue:
             raise Exception("The stack is empty.")
 
 
+    def top(self):
+        if not self.is_empty():
+            return self.queue[0]
+        else:
+            raise Exception("The stack is empty.")
 
 
+    def reverse(self):
+        aux_stack = Stack()
+
+        # save the queue to a stack
+        while not self.is_empty():
+            aux_stack.push(self.pop())
+
+        # pass the stack value to the queue
+        while not aux_stack.is_empty():
+            self.push(aux_stack.pop())
+
+
+
+# main
+# stack = Stack()
+# stack.push(10)
+# stack.push(20)
+# stack.push(30)
+# print(stack.peek())
+# stack.push(40)
+# stack.push(50)
+# print(stack.peek())
+
+queue = Queue()
+queue.push(10)
+queue.push(20)
+queue.push(30)
+queue.push(40)
+queue.push(50)
+# print(queue.is_empty())
+# print(queue.pop())
+# print(queue.pop())
+# print(queue.pop())
+# print(queue.pop())
+# print(queue.pop())
+
+# print(queue.show_queue())
+print(queue.top())
+queue.reverse()
+print(queue.top())

7fb404dce30ac00dbf40a2ad927ac17bc1f61fb5
Iris Leal <iris_leal_m@hotmail.com>
2022-11-16 12:24:43 -0800

    Added message when is heavy.

--- cracking_practices/avl_trees_exercises/avl_trees_exercises.py
+++ cracking_practices/avl_trees_exercises/avl_trees_exercises.py
@@ -12,13 +12,11 @@ class AVL_Tree:
         def __str__(self):
             return f"Node: {self.value}"
 
-
     def __init__(self):
         self.root = None
 
     def __str__(self):
         return f"Root: {self.root.value}, Height: {self.root.height}"
-
 
     def insert(self, value):
         new_node = self._Node(value)
@@ -43,10 +41,22 @@ class AVL_Tree:
                 else:
                     traverse(current.right)
 
+            # height calculation
             left_height = 0 if current.left is None else current.left.height
             right_height = 0 if current.right is None else current.right.height
 
             current.height = (max(left_height, right_height)) + 1
+
+            # Check if the tree is unbalanced
+            # balanceFactor = height(L) - heigh(Right)
+            # > 1  => Left heavy, so rotate Right
+            # < -1 => Right heavy, so rotate Left
+            balanceFactor = left_height - right_height
+
+            if balanceFactor > 1:
+                print("   => Left heavy, rotate Right")
+            if balanceFactor <= -1:
+                print("Node: ", current.value, ", height:", current.height, "   => Right heavy, rotate Left")
 
         traverse(self.root)
 
@@ -58,10 +68,11 @@ if __name__ == "__main__":
     os.system("clear")
     avl = AVL_Tree()
     avl.insert(10)
-    avl.insert(5)
+    # avl.insert(5)
     avl.insert(20)
     avl.insert(30)
-    avl.insert(40)
+    # avl.insert(40)
+    # avl.insert(50)
     print(avl)
     # print(avl.root.left.value)
     # print(avl.root.left.left.value)

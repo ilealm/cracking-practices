451718d73b4868584ceb796ea7f89723e61cf0be
Iris Leal <iris_leal_m@hotmail.com>
2022-11-23 08:19:11 -0800

    Added remove method working only when a parent has two children. Rest functionality will be added.

--- cracking_practices/heap_exercises/heap_exercises.py
+++ cracking_practices/heap_exercises/heap_exercises.py
@@ -9,22 +9,24 @@ class HeapMax:
         return str(self.array)
 
     def get_last_index(self):
-        return len(self.array)-1
+        return len(self.array) - 1
 
     def _get_parent_index(self, child_index):
         return int((child_index - 1) / 2)
 
     def _need_to_bubble_up(self, child_index):
         parent_index = self._get_parent_index(child_index)
-        print("parent_index ", parent_index)
 
-        if parent_index < 0 : return False
+        if parent_index < 0:
+            return False
 
         return int(self.array[child_index]) > int(self.array[parent_index])
 
     def _bubble_up(self, parent_index, child_index):
-        self.array[parent_index], self.array[child_index] =  self.array[child_index], self.array[parent_index]
-
+        self.array[parent_index], self.array[child_index] = (
+            self.array[child_index],
+            self.array[parent_index],
+        )
 
     def add(self, value):
         self.array.append(value)
@@ -35,17 +37,137 @@ class HeapMax:
             self._bubble_up(parent_index, current_index)
             current_index = parent_index
 
+    def get_root(self):
+        if self.is_array_empty():
+            return -1
 
-    def delete(self):
-        pass
+        return self.array[0]
+
+    def is_array_empty(self):
+        return len(self.array) == 0
+
+    def _get_left_children_index(self, parent_index):
+        return (parent_index * 2) + 1
+
+    def _get_right_children_index(self, parent_index):
+        return (parent_index * 2) + 2
+
+    def parent_has_two_children(self, left_childen_index, right_childen_index):
+        return (left_childen_index and right_childen_index) < self.get_last_index()
+
+
+    # def is_parent_smaller(self, left_childen_index, right_childen_index):
+    #     return (left_childen_index and right_childen_index) < self.get_last_index()
+
+
+    def get_index_bigger_sibling(self, left_childen_index, right_childen_index):
+        return (
+            left_childen_index
+            if (self.array[left_childen_index] > self.array[right_childen_index])
+            else right_childen_index
+        )
+
+    def _bubble_down(self, parent_index, child_index):
+        self.array[parent_index], self.array[child_index] = (
+            self.array[child_index],
+            self.array[parent_index],
+        )
+
+    def _do_bubble_down(self, parent_index):
+        left_childen_index = self._get_left_children_index(parent_index)
+        right_childen_index = self._get_right_children_index(parent_index)
+
+        if self.parent_has_two_children(left_childen_index, right_childen_index):
+            bigger_index = self.get_index_bigger_sibling(
+                left_childen_index, right_childen_index
+            )
+            self._bubble_down(parent_index, bigger_index)
+
+            return bigger_index
+
+    #     # right
+    #     if right_childen_index <= self.get_last_index():
+    #         self._bubble_down(parent_index, right_childen_index)
+    #         # print(self.array[right_childen_index], " is bigger, and need to bubble down")
+    #         return right_childen_index
+
+    def _need_to_bubble_down(self, parent_index):
+        left_childen_index = self._get_left_children_index(parent_index)
+        right_childen_index = self._get_right_children_index(parent_index)
+        print(
+            "parent_index ",
+            parent_index,
+            ", left_childen_index, ",
+            left_childen_index,
+            ", right_childen_index",
+            right_childen_index,
+        )
+
+        # check if any of its children has a bigger value than the parent
+        # print(self.get_last_index())
+        if self.parent_has_two_children(left_childen_index, right_childen_index):
+                # print("has 2 children")
+                greater = max(
+                    self.array[left_childen_index], self.array[left_childen_index]
+                )
+                if greater > self.array[parent_index]:
+                    print('need to bubble down with one children')
+                    return True
+
+        # todo when only has 1 children
+        # if right_childen_index <= self.get_last_index():
+        #     if self.array[parent_index] < self.array[right_childen_index]:
+        #         return True
+
+        #     if left_childen_index <= self.get_last_index():
+        #         if self.array[parent_index] < self.array[left_childen_index]:
+        #             return True
+
+        return False
+
+    def remove(self):
+        if self.is_array_empty():
+            return -1
+
+        root = self.get_root()
+
+        # validate if array only has 1 element
+        if self.get_last_index() > 0:
+            parent_index = 0
+            self.array[parent_index] = self.array.pop()
+            while self._need_to_bubble_down(parent_index):
+                print("Bublle down on ", self.array[parent_index])
+                parent_index = self._do_bubble_down(parent_index)
+        else:
+            self.array.clear()
+            print("all array cleared")
+
+        return root
 
 
 if __name__ == "__main__":
     os.system("clear")
     heap_max = HeapMax()
+    # mosh example
+    # heap_max.add(15)
+    # heap_max.add(10)
+    # heap_max.add(3)
+    # heap_max.add(8)
+    # heap_max.add(12)
+    # heap_max.add(9)
+    # heap_max.add(4)
+    # heap_max.add(1)
+    # heap_max.add(24)
+
+    # delete example
     heap_max.add(15)
     heap_max.add(10)
-    heap_max.add(3)
+    heap_max.add(9)
     heap_max.add(8)
-    heap_max.add(12)
+    heap_max.add(5)
+    heap_max.add(3)
+    heap_max.add(4)
+    print("original")
+    print(heap_max)
+    print("heap removed: ", heap_max.remove())
     print(heap_max)

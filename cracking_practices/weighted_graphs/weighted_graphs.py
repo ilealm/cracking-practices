b705544b6bdef625c0f558494dcba54f221e528d
Iris Leal <iris_leal_m@hotmail.com>
2023-05-23 19:57:10 -0700

    Restructured classes organization. Now, there are no more nested classes.

--- cracking_practices/weighted_graphs/weighted_graphs.py
+++ cracking_practices/weighted_graphs/weighted_graphs.py
@@ -1,63 +1,67 @@
+class Edge:
+    def __init__(self, _from, to, weight):
+        self._from = _from
+        self.to = to
+        self.weight = weight
+
+    def __repr__(self):
+        return f"{self.to} : {self.weight}"
+
+    def __str__(self):
+        return f"{self.to} : {self.weight}"
+
+
+class Node(Edge):
+    def __init__(self, label):
+        self.label = label
+        # each node will store its adj_list
+        self.edges_list = []
+
+    def __repr__(self):
+        return f"{self.label}"
+
+    def __str__(self):
+        return f"{self.label}"
+
+    def add_edge(self, _from, to, weight):
+        new_edge = Edge(_from, to, weight)
+
+        self.edges_list.append(new_edge)
+
+
 class WightedGraph:
-    class Node:
-        def __init__(self, label):
-            self.label = label
-
-        def __repr__(self):
-            # def __str__(self):
-            return f"{self.label}"
-
-    class Edge:
-        # from and to are Node objects
-        def __init__(self, _from, to, weight):
-            self._from = _from
-            self.to = to
-            self.weight = weight
-
-        # def __repr__(self):
-        def __str__(self):
-            # return f"{self._from} -> {self.to}"
-            return f"{self._from} -> {self.to} : {self.weight}"
-
     def __init__(self):
         self.nodes = {}
-        # node  : list of edges:
-        self.adj_list = {}
 
     def _key_already_exist_in_nodes(self, key):
         return key in self.nodes.keys()
 
     def _key_already_exist_in_adj_list(self, key):
-        return key in self.adj_list.keys()
+        # todo fix this
+        return False
+        # return key in self.adj_list.keys()
 
     def add_node(self, label):
-        new_node = self.Node(label)
+        new_node = Node(label)
         if not self._key_already_exist_in_nodes(label):
             self.nodes[label] = new_node
 
-        if not self._key_already_exist_in_adj_list(label):
-            self.adj_list[new_node] = []
-
     def add_edge(self, _from, to, weight):
-        # # this graph is undirected, so I need to have the edges _from -> to, and to -> _from
-
         node_from = self.nodes.get(_from)
         node_to = self.nodes.get(to)
 
         if node_to is None or node_from is None:
             return
 
-        edge_from = self.Edge(node_from, node_to, weight)
-        edge_to = self.Edge(node_to, node_from, weight)
-
-        self.adj_list.get(node_from).append(edge_from)
-        self.adj_list.get(node_to).append(edge_to)
+        node_from.add_edge(node_from, node_to, weight)
+        node_to.add_edge(node_to, node_from, weight)
 
     def print_edges(self):
-        for node, adj_list in self.adj_list.items():
-            print(node, "connections:")
-            for edge in adj_list:
-                print("    ", edge)
+        for node, edges in self.nodes.items():
+            # print(node)
+            for edge in edges.edges_list:
+                print(node, "->", edge)
+
 
 
 if __name__ == "__main__":
@@ -69,7 +73,6 @@ if __name__ == "__main__":
     g.add_node("A")
     g.add_node("B")
     g.add_node("C")
-    # g.add_node('D')
 
     g.add_edge("A", "B", 3)
     g.add_edge("A", "C", 2)
